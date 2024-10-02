e46960910327912aaaf9ebfd531e01e695d929fb
Jon Bjarne Bø <77270569+Z0XZ@users.noreply.github.com>
2024-10-02 08:51:03 +0200

    Create andregradslikning_test.py

---
+++ andregradslikning_test.py
@@ -0,0 +1,31 @@
+import andregradslikning
+
+import pytest
+import random
+import math
+
+# (Inkluder nullpunkt-funksjonen her)
+
+def generer_tilfeldige_koeffisienter():
+    """ Hjelpefunksjon for å generere tilfeldige koeffisienter. """
+    a = random.randint(-100, 100)/10
+    if round(a, 2) == 0:
+        a += 1
+    b = random.randint(-100, 100)/10
+    c = random.randint(-100, 100)/10
+    if random.randint(1,20) == 1:
+        c = b**2 / (4*a)
+    return a, b, c
+    
+@pytest.mark.parametrize("a,b,c", [generer_tilfeldige_koeffisienter() for _ in range(100)])
+def test_tilfeldige_nullpunkt(a, b, c):
+    """ Tester nullpunkt-funksjonen med tilfeldig genererte koeffisienter. """
+    discriminant = b**2 - 4*a*c
+    expected = "Likningen har ingen løsninger!"
+    if discriminant > 0:
+        expected = (round((-b + math.sqrt(discriminant)) / (2 * a),2), 
+                    round((-b - math.sqrt(discriminant)) / (2 * a),2))
+    elif discriminant == 0:
+        expected = round(-b / (2 * a), 2)
+    
+    assert andregradslikning.losninger(a, b, c) == expected
